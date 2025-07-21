e9a15ed75f5f8dcff3eedb22696c075e7d758112
gioZAK <giovane.zakc@gmail.com>
2025-07-21 20:47:07 +0100

    Add .env support and prep for deployment

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,7 @@
-// client/src/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001', {
-  transports: ['websocket']
+const socket = io(process.env.REACT_APP_SERVER_URL, {
+  transports: ['websocket'],
 });
 
 export default socket;
