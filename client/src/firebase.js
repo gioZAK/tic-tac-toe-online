7cb4c39c3cb22d41fc53d37e54410312159a4115
gioZAK <giovane.zakc@gmail.com>
2025-07-24 21:21:51 +0100

    Fix file name and missed install

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -9,10 +9,11 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
 export { app, auth, db };
